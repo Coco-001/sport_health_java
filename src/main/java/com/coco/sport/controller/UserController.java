e05d048ef645e9fcdc28ef730602686a799abc6e
Coco-001 <1131395889@qq.com>
2022-07-05 20:53:06 +0800

    引入redis实现缓存

--- src/main/java/com/coco/sport/controller/UserController.java
+++ src/main/java/com/coco/sport/controller/UserController.java
@@ -4,10 +4,13 @@ import com.alibaba.fastjson.JSON;
 import com.coco.sport.dao.UserDao;
 import com.coco.sport.entity.QueryInfo;
 import com.coco.sport.entity.User;
+import com.coco.sport.util.RedisUtil;
 import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import java.io.Console;
 import java.util.HashMap;
 import java.util.List;
@@ -22,7 +25,107 @@ import java.util.Map;
  */
 @RestController
 public class UserController {
+//
+//
+//    @Resource
+//    private UserDao userDao;
+//
+//    @Autowired
+//    private RedisUtil redisUtil;
+//
+//    @RequestMapping("/allUser")
+//    public String getUserList(QueryInfo queryInfo) {
+//
+//        //用户总数
+//        //int userCounts = userDao.getUserCounts("%" + queryInfo.getQuery() + "%");
+//        //先从从redis中查询，如果查询结果为空，再从数据库中查询，把结果存入redis
+//        Integer userCounts = (Integer) redisUtil.get("userCounts");
+//        if (null == userCounts) {
+//            userCounts = userDao.getUserCounts("%" + queryInfo.getQuery() + "%");
+//            redisUtil.set("userCounts", userCounts);
+//        }
+//
+//        //从第几页开始
+//        int pageStart = (queryInfo.getPageNum() - 1) * queryInfo.getPageSize();
+//
+//        //查询用户列表，@用户名模糊查询，@开始页，@页面大小
+//        //List<User> userList = userDao.getAllUser("%" + queryInfo.getQuery() + "%", pageStart, queryInfo.getPageSize());
+//        //先从从redis中查询，如果查询结果为空，再从数据库中查询，把结果存入redis
+//        List<User> userList = (List<User>) redisUtil.get("allUser");
+//        if (CollectionUtils.isEmpty(userList)) {
+//            userList = userDao.getAllUser("%" + queryInfo.getQuery() + "%", pageStart, queryInfo.getPageSize());
+//            redisUtil.set("allUser", userList);
+//            //单个用户数据也存一份
+//            for (User user : userList) redisUtil.set(String.valueOf(user.getId()), user);
+//        }
+//
+//        Map<String, Object> res = new HashMap<>();
+//
+//        res.put("userCounts", userCounts);
+//        res.put("userList", userList);
+//
+//        return JSON.toJSONString(res);
+//    }
+//
+//    @RequestMapping("/userState")
+//    public String updateState(@RequestParam("id") Integer id, @RequestParam("state") Boolean state) {
+//        int res = userDao.updateState(id, state);
+//        return res > 0 ? "success" : "error";
+//    }
+//
+//    @RequestMapping("/addUser")
+//    public String addUser(@RequestBody User user) {
+//        user.setRole("普通用户");
+//        user.setState(false);
+//        int i = userDao.addUser(user);
+//        List<User> userList = (List<User>) redisUtil.get("allUser");
+//        userList.add(user);
+//        redisUtil.set("allUser", userList);
+//        redisUtil.set(String.valueOf(user.getId()), user);
+//        return i > 0 ? "success" : "error";
+//    }
+//
+//    @RequestMapping("/deleteUser")
+//    public String deleteUser(@RequestParam("id") Integer id) {
+//        //从数据库中删除
+//        int i = userDao.deleteUser(id);
+//        //从缓存中移除
+//        User user = (User) redisUtil.get(String.valueOf(id));
+//        List<User> userList = (List<User>) redisUtil.get("allUser");
+//        userList.remove(userList.indexOf(user));
+//        redisUtil.del(String.valueOf(id));
+//        redisUtil.set("allUser", user);
+//        return i > 0 ? "success" : "error";
+//    }
 
+//    @RequestMapping("/getUser")
+//    public String getUser(@RequestParam("id") Integer id) {
+//        //从缓存查
+//        User user = (User) redisUtil.get(String.valueOf(id));
+//        //缓存中没有
+//        if (null == user) {
+//            //从数据库查
+//            user = userDao.getUser(id);
+//            //写入缓存
+//            redisUtil.set(String.valueOf(user.getId()), user);
+//        }
+//        return JSON.toJSONString(user);
+//    }
+
+//    @RequestMapping("/updateUser")
+//    public String updateUser(@RequestBody User user) {
+//        User u = (User) redisUtil.get(String.valueOf(user.getId()));
+//        List<User> userList = (List<User>) redisUtil.get("allUser");
+//        Integer idx = userList.indexOf(u);
+//        //更新写入缓存
+//        redisUtil.set(String.valueOf(user.getId()), user);
+//        userList.set(idx, user);
+//        redisUtil.set("allUser", userList);
+//        //更新写入数据库
+//        int i = userDao.updateUser(user);
+//        return i > 0 ? "success" : "error";
+//    }
+//
     @Autowired
     private UserDao userDao;
 
@@ -36,6 +139,7 @@ public class UserController {
 
         //查询用户列表，@用户名模糊查询，@开始页，@页面大小
         List<User> userList = userDao.getAllUser("%" + queryInfo.getQuery() + "%", pageStart, queryInfo.getPageSize());
+
 
         Map<String, Object> res = new HashMap<>();
 
---
+++ src/main/java/com/coco/sport/util/redisConfig.java
@@ -0,0 +1,78 @@
+package com.coco.sport.util;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+/**
+ * @Author: Mr.Shan
+ * @Date: 2022/7/5 8:55
+ * @ClassName: redisConfig
+ * @Description: redis配置类
+ * @Version: v1.0
+ */
+//开启缓存
+@EnableCaching
+@Configuration
+public class redisConfig extends CachingConfigurerSupport {
+    @Bean
+    @SuppressWarnings("all")
+    //redis通用配置模板
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String,Object> template = new RedisTemplate<>();
+       template.setConnectionFactory(factory);
+
+        // 指定kv的序列化方式
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+
+        // key采用String的序列化方式
+        template.setKeySerializer(stringRedisSerializer);
+        // hash的key也采用String的序列化方式
+        template.setHashKeySerializer(stringRedisSerializer);
+        // value序列化方式采用jackson
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        // hash的value序列化方式采用jackson
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+        template.afterPropertiesSet();
+
+        return template;
+    }
+
+    @Bean
+    @SuppressWarnings("all")
+    public CacheManager cacheManager(RedisConnectionFactory factory) {
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        //解决查询缓存转换异常的问题
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        // 配置序列化（解决乱码的问题）,过期时间600秒
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(600))  //设置数据过期时间600秒
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer))
+                .disableCachingNullValues();
+        RedisCacheManager cacheManager = RedisCacheManager.builder(factory)
+                .cacheDefaults(config)
+                .build();
+        return cacheManager;
+    }
+}
